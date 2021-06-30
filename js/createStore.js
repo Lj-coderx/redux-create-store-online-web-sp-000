1928253b4467038a2b7547a80e745e4e433fef30
Lj <tnedutsl457892@gmail.com>
2021-06-30 11:17:10 +0000

    Automatically backed up by Learn

--- js/createStore.js
+++ js/createStore.js
@@ -1,24 +1,38 @@
-let state;
-
+function createStore() {
+  let state;
+ 
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+ 
+  function getState() {
+    return state;
+  }
+ 
+  return {
+    dispatch,
+    getState
+  };
+};
+ 
 function reducer(state = { count: 0 }, action) {
-  switch (action.type) {
+   switch (action.type) {
     case 'INCREASE_COUNT':
-      return { count: state.count + 1 };
-
+       return { count: state.count + 1 };
+ 
     default:
       return state;
   }
 };
-
-let store = createStore();
-store .dispatch({ type: '@@INIT' });
-
+ 
 function render() {
    let container = document.getElementById('container');
      container.textContent = store.getState().count;
 };
-
-dispatch({ type: '@@INIT' })
+ 
+let store = createStore();
+store .dispatch({ type: '@@INIT' });
 let  button = document.getElementById('button');
  
 button .addEventListener('click', () => {
